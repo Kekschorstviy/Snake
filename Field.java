50b8ef4975ffb82a2899980d98cd221f0fc64896
Kekschorstviy <117016566+Kekschorstviy@users.noreply.github.com>
2022-11-16 19:05:44 +0100

    Update Field.java

--- Field.java
+++ Field.java
@@ -11,200 +11,180 @@ import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class Field extends JPanel implements ActionListener
-{
-	
+public class Field extends JPanel implements ActionListener {
+
 	private static final long serialVersionUID = 1L;
-	private final int SIZE = 320;
-	private final int S_SIZE = 16; 
-	private final int ALL_S = 400;
-	
-	private int fruitX; //fruit coordinates
-	private int fruitY; 
+	private final int SIZE = 500;
+	private final int S_SIZE = 16;
+	private final int ALL_S = 500;
+
+	private int fruitX; // fruit coordinates
+	private int fruitY;
 	private int[] x = new int[ALL_S];
 	private int[] y = new int[ALL_S];
 	private int snakie;
-	
+
 	private Timer timer;
 	private Image snake;
 	private Image fruit;
-	
-	private boolean left; 
-	private boolean up; 
-	private boolean down; 
-	private boolean Game = true ;
-	private boolean right= true ; 
-	private int score; 
-	
-	
-	class FieldKeyListener extends KeyAdapter{
-	       
-	        public void keyPressed(KeyEvent e) {
-	        	
-	            super.keyPressed(e);
-	            int key = e.getKeyCode();
-	            
-	            if(key == KeyEvent.VK_RIGHT && !left)
-	            {
-	                right = true;
-	                up = false;
-	                down = false;
-	            }
-	            
-	            if(key == KeyEvent.VK_LEFT && !right)
-	            {
-	                left = true;
-	                up = false;
-	                down = false;
-	            }
-	           
 
-	            if(key == KeyEvent.VK_UP && !down)
-	            {
-	            	up = true;
-	            	right = false;
-	               
-	                left = false;
-	            }
-	            if(key == KeyEvent.VK_DOWN && !up)
-	            {
-	                right = false;
-	                down = true;
-	                left = false;
-	            }
-	        }
-	        	
+	private boolean left;
+	private boolean up;
+	private boolean down;
+	private boolean Game = true;
+	private boolean right;
+	private int score;
+
+	class FieldKeyListener extends KeyAdapter {
+
+		public void keyPressed(KeyEvent e) {
+
+			super.keyPressed(e);
+			int key = e.getKeyCode();
+
+			if (key == KeyEvent.VK_RIGHT && !left) {
+				right = true;
+				up = false;
+				down = false;
+			}
+
+			if (key == KeyEvent.VK_LEFT && !right) {
+				left = true;
+				up = false;
+				down = false;
+			}
+
+			if (key == KeyEvent.VK_UP && !down) {
+				up = true;
+				right = false;
+
+				left = false;
+			}
+			if (key == KeyEvent.VK_DOWN && !up) {
+				right = false;
+				down = true;
+				left = false;
+			}
+		}
+
 	}
-		
-	public void loadImage () // method to load the  snake pattern and fruit image 
+
+	public void loadImage() // method to load the snake pattern and fruit image
 	{
 		ImageIcon iia = new ImageIcon("fruit.png");
 		fruit = iia.getImage();
-		
+
 		ImageIcon iid = new ImageIcon("snake.png");
 		snake = iid.getImage();
 	}
-	
-	protected void paintComponent(Graphics g) {
-		
+
+	protected void paintComponent(Graphics g) // method to draw some components , score and fruit
+	{
 		super.paintComponent(g);
-		if(Game) 
-		{
-			g.drawString("Score:     " + score, 20, 30); 
+		if (Game) {
+			g.drawString("Score:     " + score, 20, 20);
 			g.drawImage(fruit, fruitX, fruitY, this);
-			for(int i = 0; i < snakie; i++) {
+			for (int i = 0; i < snakie; i++) {
 				g.drawImage(snake, x[i], y[i], this);
-				
+
 			}
 		}
 		if (!Game) {
-			g.drawString("GAME OVER", 200, 150);
+			g.drawString("GAME OVER", 210, 150);
+			g.drawString("Your score:     " + score, 200, 200);
 			
+
 		}
 	}
-	
+
 	public void createFruit() 
 	{
-		
-		fruitX = new Random().nextInt(20)*S_SIZE; // 20 because only 20 fruits can be placed on the game field
-		fruitY = new Random().nextInt(20)*S_SIZE;
-		
+
+		fruitX = new Random().nextInt(20) * S_SIZE; // 20 because only 20 fruits can be placed on the game field
+		fruitY = new Random().nextInt(20) * S_SIZE;
+
 	}
-	
-	public void InGame()
-	{
-		
+
+	public void InGame() // method which describes what actions shall be performed while in game 
+{
+
 		snakie = 3;
-		for(int i = 0; i < snakie; i++) 
-		{
-			x[i] = 48 - i*S_SIZE;
+		for (int i = 0; i < snakie; i++) {
+			x[i] = 48 - i * S_SIZE;
 			y[i] = 48;
 		}
 		timer = new Timer(250, this);
 		timer.start();
 		createFruit();
 	}
-	
-	public void Moving()  // to define snake moves 
+
+	public void Moving() // to define snake moves
 	{
-		for (int i = snakie; i > 0 ; i--) 
-		{
-			x[i] = x[i-1];
-			y[i] = y[i-1];
+		for (int i = snakie; i > 0; i--) {
+			x[i] = x[i - 1];
+			y[i] = y[i - 1];
 		}
-		if (left) 
-		{
+		if (left) {
 			x[0] -= S_SIZE;
 		}
-		
-		if (right) 
-		{
+
+		if (right) {
 			x[0] += S_SIZE;
 		}
-		
-		if (down) 
-		{
+
+		if (down) {
 			y[0] += S_SIZE;
 		}
-		
-		if (up) 
-		{
+
+		if (up) {
 			y[0] -= S_SIZE;
 		}
 	}
-	
-	public void checkFruit() 
+
+	public void checkFruit() // Method to check if the fruit got eaten
 	{
-		if (x[0] == fruitX ) 
-		{ 
-			if(y[0] == fruitY) 
-			{
-			
-				snakie ++;
-				score ++;
+		if (x[0] == fruitX) {
+			if (y[0] == fruitY) {
+
+				snakie++;
+				score++;
 				createFruit();
-		}
 			}
-			
 		}
-	
-	public void Collision() // method to check if snake met itself or one of the walls 
-	{ 
-		
-		for(int i = snakie; i>0; i--) 
-		{
-			if (i > 4 ) { // in case of 4 snake can eat it self 
-				if(x[0] == x[i]) {
-					if(y[0] == y[i]) {
+
+	}
+
+	public void Collision() // method to check if snake met itself or one of the walls
+	{
+
+		for (int i = snakie; i > 0; i--) {
+			if (i > 4) { // in case of 4 snake can eat it self
+				if (x[0] == x[i]) { // snake meets its self
+					if (y[0] == y[i]) { //snake meets its self
 						Game = false;
 					}
 				}
 			}
 		}
-		
-		if (x[0] > SIZE) 
-		{
+
+		if (x[0] > SIZE) {
 			Game = false;
 		}
-		
-		if (y[0] > SIZE) 
-		{
+
+		if (y[0] > SIZE) {
 			Game = false;
 		}
-		
-		if (x[0] < 0) 
-		{
+
+		if (x[0] < 0) {
 			Game = false;
 		}
-		
-		
-		if (y[0] < 0) 
-		{
+
+		if (y[0] < 0) {
 			Game = false;
 		}
-		
+
 	}
-	
+
 	public Field() // constructor to set the background , load images and initialize InGame method
 	{
 		setBackground(Color.ORANGE);
@@ -212,21 +192,18 @@ public class Field extends JPanel implements ActionListener
 		InGame();
 		addKeyListener(new FieldKeyListener());
 		setFocusable(true);
-		
+
 	}
 
-	public void actionPerformed(ActionEvent e)  //method which defines actions performed while timer is running 
+	public void actionPerformed(ActionEvent e) // method which defines actions performed while timer is running
 	{
-		if (Game) 
-		{
+		if (Game) {
 			Moving();
 			checkFruit();
 			Collision();
-			
+
 		}
 		repaint();
 	}
-	
 
 }
-
